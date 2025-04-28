55117e5e8f044831761c75bf58b899d6c995d863
David <128511591+chaos-hunter@users.noreply.github.com>
2025-04-28 11:22:43 -0400

    Add files via upload

---
+++ include/VCHelpers.h
@@ -0,0 +1,8 @@
+
+#include "VCParser.h"  
+
+Card* alternate();
+VCardErrorCode setfn(Card* obj, const char* fn);
+char* getfn(const Card* obj);
+char* getbday(const Card* obj);
+char* getanniv(const Card* obj);
